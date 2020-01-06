f8c17c6ce57df7ea6cc6703dee8afaca3c6c4406
jweir136 <jweir136@yahoo.com>
2020-01-06 12:16:27 -0500

    add Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    Integer[] arr = {1, 3, 5, 4};
+    MaxHeap heap = new MaxHeap();
+
+    for (Integer i : arr)
+      heap.insert(i);
+  }
+}
