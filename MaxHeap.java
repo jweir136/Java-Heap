d1a8c271b9dc23da3199b4c01672d935465ce07e
jweir136 <jweir136@yahoo.com>
2020-01-15 22:44:11 -0500

    refactor the insert() method

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 
 public class MaxHeap {
@@ -10,20 +9,18 @@ public class MaxHeap {
     this.size = 0;
     this.arr.add(Integer.MAX_VALUE); // make sure that the first position can never be used.
   }
+
   public int parent(int pos){
     return pos/2;
   }
-  public void insert(Comparable value) {
-    //size++;
-    this.arr.add(value);
-    System.out.println("inserting " +this.arr.size());
-    int current = this.arr.size()-1;
-    while(this.arr.get(current).compareTo(this.arr.get(parent(current) )) > 0){
-      swap(current,parent(current));
-      current = parent(current);
-    }
 
+  public void insert(Comparable value) {
+    this.arr.add(value);
+
+    for (int i = this.arr.size() - 1; this.arr.get(i).compareTo(this.arr.get(parent(i))) > 0; i = parent(i))
+      swap(i, parent(i));
   }
+
   public void print() {
     for (int i = 1; i <= this.arr.size()-1; i++) {
       System.out.println(" node : " + this.arr.get(i));
